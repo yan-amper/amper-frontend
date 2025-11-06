830fe85971faf295684a4088294f01034ac39932
Yan Turnt <bx6xbchosen1@gmail.com>
2025-11-06 18:50:14 +0500

    feat: client rendering for popular batteries

--- src/app-pages/home/index.tsx
+++ src/app-pages/home/index.tsx
@@ -6,16 +6,18 @@ import {
   RecommendedBatteries,
   Slider,
 } from "./ui";
+import { ProductsApi } from "@/entities";
 
 export const HomePage = async () => {
   const banners = await BannersApi.getBanners();
+  const popularProducts = await ProductsApi.getPopularProducts();
 
   return (
     <>
       <Slider banners={banners} />
       <RecommendedBatteries />
       <Catalog />
-      <PopularBatteries />
+      <PopularBatteries products={popularProducts} />
       <Address />
     </>
   );

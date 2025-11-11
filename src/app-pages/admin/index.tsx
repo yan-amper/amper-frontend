33bf546afb813981521a2d692d10df731c7eb62e
Yan Turnt <bx6xbchosen1@gmail.com>
2025-11-11 16:28:59 +0500

    feat: state

--- src/app-pages/admin/index.tsx
+++ src/app-pages/admin/index.tsx
@@ -5,7 +5,7 @@ import RequestsPage, { RequestsPageProps } from "../requests";
 import { LoginPage } from "../login";
 
 export const AdminPage = ({ requests, products }: RequestsPageProps) => {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
 
   return show ? (
     <RequestsPage requests={requests} products={products} />

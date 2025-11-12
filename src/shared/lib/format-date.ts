5a750fb48a95d7c29219d8529a40bbb98f560200
Yan Turnt <bx6xbchosen1@gmail.com>
2025-11-13 00:39:51 +0500

    feat: utc +3 time

--- src/shared/lib/format-date.ts
+++ src/shared/lib/format-date.ts
@@ -1,5 +1,8 @@
 export const formatDate = (dateString: string) => {
   const date = new Date(dateString);
+
+  date.setHours(date.getHours() + 3);
+
   return date.toLocaleDateString("ru-RU", {
     year: "numeric",
     month: "long",

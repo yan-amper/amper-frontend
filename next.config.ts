7c032af40361436edd4427c159c426c6a726b232
Yan Turnt <bx6xbchosen1@gmail.com>
2026-05-24 17:25:37 +0500

    fix: start dev bug

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,22 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 import { apiUrl } from "@/shared";
 import type { NextConfig } from "next";
+
+const projectRoot = fs.realpathSync(
+  path.dirname(fileURLToPath(import.meta.url)),
+);
 
 if (!apiUrl) {
   console.warn("apiUrl не указан");
 }
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: projectRoot,
+  },
+  outputFileTracingRoot: projectRoot,
   productionBrowserSourceMaps: false,
   compiler: {
     styledComponents: true,
